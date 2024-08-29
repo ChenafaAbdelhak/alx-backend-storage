8d5d28fcc5911c243405e367c46323d691300b64
hr <abdelhak.chenafa@univ-temcen.dz>
2024-08-29 01:58:33 +0100

    retrieve function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """module for cache class"""
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,22 @@ class Cache:
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
+
+    def get(self, key: str, fn: Callabale = None)-> Union[str, bytes, int, float]:
+        """retrieve a value from redis storage"""
+        value = self._redis.get(key)
+        return fn(value) if fn is not None else value
+
+    def get_str(self, key: str) -> str:
+        """Retrieves a string value from a Redis data storage."""
+        data = self._redis.get(key)
+        if data is not None:
+            return data.decode("utf-8")
+        return None
+    
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value from a Redis data storage."""
+        data = self._redis.get(key)
+        if data is not None:
+            return int(data)
+        return None
